e7420d1ca1b9735614e972319f5602925f2b4ace
AndresCampo <andrescampoe@gmail.com>
2018-05-28 20:44:38 -0500

    first release

--- app/src/main/java/com/movil/clase/tallercomputadores/CrearPC.java
+++ app/src/main/java/com/movil/clase/tallercomputadores/CrearPC.java
@@ -1,17 +1,23 @@
 package com.movil.clase.tallercomputadores;
 
+import android.content.Intent;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
+
+import java.security.Principal;
+import java.util.ArrayList;
 
 public class CrearPC extends AppCompatActivity {
     private String id;
     private Spinner cmbMarca,cmbTipo,cmbRam,cmbColor,cmbSo;
     private ArrayAdapter<String> adapterMarca,adapterTipo,adapterRam,adapterColor,adapterSo;
     private String[] opcMarca,opcTipo,opcRam,opcColor,opcSo;
-    private ArrayAdapter<Integer> fotos;
+    private ArrayList<Integer> fotos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,18 +35,47 @@ public class CrearPC extends AppCompatActivity {
         opcTipo = this.getResources().getStringArray(R.array.tipos);
         opcRam = this.getResources().getStringArray(R.array.rams);
         opcColor = this.getResources().getStringArray(R.array.colores);
-        opcSo = this.getResources().getStringArray(R.array.colores);
+        opcSo = this.getResources().getStringArray(R.array.sos);
 
         adapterMarca = new ArrayAdapter<>(this,android.R.layout.simple_spinner_item,opcMarca);
         adapterTipo = new ArrayAdapter<>(this,android.R.layout.simple_spinner_item, opcTipo);
         adapterRam = new ArrayAdapter<>(this,android.R.layout.simple_spinner_item,opcRam);
         adapterColor = new ArrayAdapter<>(this,android.R.layout.simple_spinner_item,opcColor);
-        adapterSo = new ArrayAdapter<>(this,android.R.layout.simple_spinner_item, opcColor);
+        adapterSo = new ArrayAdapter<>(this,android.R.layout.simple_spinner_item, opcSo);
 
         cmbMarca.setAdapter(adapterMarca);
         cmbTipo.setAdapter(adapterTipo);
         cmbRam.setAdapter(adapterRam);
         cmbColor.setAdapter(adapterColor);
         cmbSo.setAdapter(adapterSo);
+
+        fotos = new ArrayList<>();
+        fotos.add(R.drawable.images1);
+        fotos.add(R.drawable.images2);
+        fotos.add(R.drawable.images3);
+    }
+
+    public void guardar(View v){
+        int marca,tipo,ram,color,so, foto;
+
+        foto = Datos.fotoAleatoria(fotos);
+        marca = cmbMarca.getSelectedItemPosition();
+        tipo = cmbTipo.getSelectedItemPosition();
+        ram = cmbRam.getSelectedItemPosition();
+        color = cmbColor.getSelectedItemPosition();
+        so = cmbSo.getSelectedItemPosition();
+
+        id = Datos.getId();
+
+        Computador c = new Computador(id,marca,tipo,ram,color,so,foto);
+        c.guardar();
+
+        Snackbar.make(v,this.getResources().getString(R.string.exitoso),Snackbar.LENGTH_SHORT).setAction("Action",null).show();
+
+    }
+    public void onBackPressed(){
+        finish();
+        Intent i = new Intent(CrearPC.this, Principal.class);
+        startActivity(i);
     }
 }
--- app/src/main/java/com/movil/clase/tallercomputadores/MainActivity.java
+++ app/src/main/java/com/movil/clase/tallercomputadores/MainActivity.java
@@ -1,5 +1,6 @@
 package com.movil.clase.tallercomputadores;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -20,14 +21,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
     }
 
     @Override
@@ -50,5 +44,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void crerPC(View v){
+        Intent i = new Intent(MainActivity.this,CrearPC.class);
+        startActivity(i);
     }
 }
